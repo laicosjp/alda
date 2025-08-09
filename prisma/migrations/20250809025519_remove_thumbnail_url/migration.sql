a5be0220d37d19b6f76882e38ca5fbd5e617d7b9
hulk-higakijin <hm385.chejptks@gmail.com>
2025-08-09 11:55:26 +0900

    feat: remove thumbnail-url

---
+++ prisma/migrations/20250809025519_remove_thumbnail_url/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `thumbnailUrl` on the `Lesson` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "public"."Lesson" DROP COLUMN "thumbnailUrl";
